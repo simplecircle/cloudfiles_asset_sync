56cb7d4bb887d0b1d617cac6b3db5df3ca4846e1
Elliott Golden <studio@simplecircle.net>
2013-04-25 10:42:18 -0300

    Update cloudfiles_asset_sync.rb

--- lib/cloudfiles_asset_sync.rb
+++ lib/cloudfiles_asset_sync.rb
@@ -33,9 +33,9 @@ module CloudfilesAssetSync
 
     def asset_files
       #Dir[File.join(Rails.root, 'public', '**', '*')].reject{|file| File.directory?(file)}
-      #public = Dir[File.join(Rails.root, 'public', '**')].reject{|file| File.directory?(file)}
-      Dir[File.join(Rails.root, 'public', 'assets','**')].reject{|file| File.directory?(file)}
-      #return public + assets
+      public = Dir[File.join(Rails.root, 'public', '**')].reject{|file| File.directory?(file)}
+      assets = Dir[File.join(Rails.root, 'public', 'assets','**')].reject{|file| File.directory?(file)}
+      return public + assets
     end
 
     def upload_file(container, existing_objects, filename)
